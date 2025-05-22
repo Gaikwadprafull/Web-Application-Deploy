b61eb81a25a1a7dd57a13b1825747ba1804bb98d
Gaikwadprafull <prafullkumar.g@cloudshastrainfotech.com>
2025-05-22 19:26:46 +0530

    Update set_permissions.sh

--- scripts/set_permissions.sh
+++ scripts/set_permissions.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-chmod u+x $APP_DIR
+
+APP_DIR="/home/ec2-user/app"
+
+if [ -d "$APP_DIR" ]; then
+    chmod +x $APP_DIR/*.sh
+else
+    echo "Directory $APP_DIR does not exist."
+    exit 1
+fi
