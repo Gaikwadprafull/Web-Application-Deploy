63337ddbd1b7eecb6c22f0a62cf8c2c3acf1515a
Gaikwadprafull <prafullkumar.g@cloudshastrainfotech.com>
2025-05-26 22:50:32 +0530

    Update make_executable.sh

--- scripts/make_executable.sh
+++ scripts/make_executable.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-chmod u+x scripts/install_dependencies.sh
-chmod u+x scripts/start_server.sh
-chmod u+x scripts/stop_server.sh
-chmod u+x scripts/set_permissions.sh
-chmod u+x scripts/env.sh
+chmod +x scripts/*.sh
