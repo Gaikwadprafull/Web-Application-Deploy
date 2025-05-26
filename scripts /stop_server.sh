37708c09c6e91ca88124fcc1e4cb112905e80082
Gaikwadprafull <prafullkumar.g@cloudshastrainfotech.com>
2025-05-27 00:57:23 +0530

    Update stop_server.sh

--- scripts /stop_server.sh
+++ scripts /stop_server.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-PID=$(pgrep -f "python3 app.py")
-if [ -n "$PID" ]; then
-  echo "Stopping Flask application with PID: $PID"
-  kill -9 $PID
-else
-  echo "No Flask application running."
-fi
+pkill -f app.py || true
