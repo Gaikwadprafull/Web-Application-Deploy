1194112ae9a79ebb074714c822f905a84d1de897
Gaikwadprafull <prafullkumar.g@cloudshastrainfotech.com>
2025-05-27 16:04:21 +0530

    Update make_executable.sh

--- scripts /make_executable.sh
+++ scripts /make_executable.sh
@@ -3,17 +3,3 @@ set -e
 
 echo "Making scripts executable..."
 chmod +x /home/ec2-user/myapp/scripts/*.sh
-#!/bin/bash
-set -e
-
-echo "[stop_server.sh] Stopping any running app..."
-
-PID=$(pgrep -f app.py || true)
-
-if [[ -n "$PID" ]]; then
-  echo "Found running app with PID: $PID"
-  kill -9 $PID
-  echo "App stopped."
-else
-  echo "No app running."
-fi
