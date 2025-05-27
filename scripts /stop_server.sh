5f2e99ca780a23160e5b80762c877c11272849f6
Gaikwadprafull <prafullkumar.g@cloudshastrainfotech.com>
2025-05-27 17:37:25 +0530

    Update stop_server.sh

--- scripts /stop_server.sh
+++ scripts /stop_server.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-set -e
-
-echo "[stop_server.sh] Stopping any running app..."
-
-PID=$(pgrep -f app.py || true)
-
-if [[ -n "$PID" ]]; then
-  echo "Found running app with PID: $PID"
-  kill -9 $PID
-  echo "App stopped."
-else
-  echo "No app running."
-fi
+pkill -f app.py || true
