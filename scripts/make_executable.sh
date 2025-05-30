3a6f708c1bd8190c6d110c099ed0724eeffdd48f
Gaikwadprafull <prafullkumar.g@cloudshastrainfotech.com>
2025-05-30 22:56:16 +0530

    Delete scripts directory

--- scripts/make_executable.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-chmod +x scripts/*.sh
-chmod +x deploy.sh
--- scripts/stop_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Stop and remove Docker container if it exists
-docker stop web-app || true
-docker rm web-app || true
