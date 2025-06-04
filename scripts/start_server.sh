33ee61bbc5f91313d8fa4fa844d75002138e425c
Gaikwadprafull <prafullkumar.g@cloudshastrainfotech.com>
2025-06-04 19:19:44 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "Starting Python app..." >> /tmp/deploy.log
 cd /home/ec2-user/myapp
-nohup python3 app.py > app.log 2>&1 &
+nohup python3 app.py > /tmp/app.log 2>&1 &
+echo "Started app.py using nohup" >> /tmp/deploy.log
