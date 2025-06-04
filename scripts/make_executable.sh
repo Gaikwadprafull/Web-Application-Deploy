7fa1a4a59d516138990308ee361ae21ac2914a65
Gaikwadprafull <prafullkumar.g@cloudshastrainfotech.com>
2025-06-04 20:06:12 +0530

    Update make_executable.sh

--- scripts/make_executable.sh
+++ scripts/make_executable.sh
@@ -1,4 +1,4 @@
 chmod +x scripts/*.sh
-git add scripts/*.sh
-git commit -m "Fix: Dockerized start_server.sh"
+git add scripts/install_dependencies.sh
+git commit -m "Update: added docker setup to install_dependencies.sh"
 git push origin dev
