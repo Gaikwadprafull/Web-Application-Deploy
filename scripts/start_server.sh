0e315213439aee332b0fe214b2125e09fcb2647d
Gaikwadprafull <prafullkumar.g@cloudshastrainfotech.com>
2025-06-04 21:33:56 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+echo "Starting Dockerized Flask app..." >> /tmp/deploy.log
 cd /home/ec2-user/myapp
-nohup python3 app.py > app.log 2>&1 &
+
+docker stop flaskapp-container || true
+docker rm flaskapp-container || true
+
+docker build -t flaskapp . >> /tmp/deploy.log 2>&1
+docker run -d --name flaskapp-container -p 5000:5000 flaskapp >> /tmp/deploy.log 2>&1
+
+echo "Flask Docker container started." >> /tmp/deploy.log
