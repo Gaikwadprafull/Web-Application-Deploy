4beb07985b3805d9ca5b28660b441a0bc965dab0
Gaikwadprafull <prafullkumar.g@cloudshastrainfotech.com>
2025-06-05 21:12:22 +0530

    Create install_cw_agent.sh

---
+++ scripts/install_cw_agent.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Installing CloudWatch Agent..."
+
+sudo yum install amazon-cloudwatch-agent -y
+
+cat <<EOF | sudo tee /opt/aws/amazon-cloudwatch-agent/bin/config.json
+{
+  "agent": {
+    "metrics_collection_interval": 60,
+    "run_as_user": "root"
+  },
+  "metrics": {
+    "append_dimensions": {
+      "InstanceId": "\${aws:InstanceId}"
+    },
+    "metrics_collected": {
+      "mem": {
+        "measurement": ["mem_used_percent"],
+        "metrics_collection_interval": 60
+      },
+      "disk": {
+        "measurement": ["used_percent"],
+        "metrics_collection_interval": 60,
+        "resources": ["/"]
+      }
+    }
+  }
+}
+EOF
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+  -a fetch-config -m ec2 \
+  -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+
+echo "CloudWatch Agent setup completed."
