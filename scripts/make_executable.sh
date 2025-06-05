3dce7068f20e93f086514327aaf5f52114fb1197
Gaikwadprafull <prafullkumar.g@cloudshastrainfotech.com>
2025-06-05 22:47:15 +0530

    Update make_executable.sh

--- scripts/make_executable.sh
+++ scripts/make_executable.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-
-# Make all shell scripts executable
 chmod +x scripts/*.sh
-
-# Add all updated shell scripts to staging
-git add scripts/*.sh
-
-# Commit changes with a clear message
-git commit -m "Update: made all scripts executable and added CloudWatch setup"
-
-# Push to the target branch (stage/dev/etc.)
+git add scripts/install_dependencies.sh
+git commit -m "Update: added docker setup to install_dependencies.sh"
 git push origin stage
